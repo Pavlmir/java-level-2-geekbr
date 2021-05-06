78fb69c766e013403c8484562631fa81af84ecc3
pavlmir <mirprost@gmail.com>
2021-05-06 14:05:48 +0300

    Урок 4. Продвинутые вопросы создания графического интерфейса

--- src/main/java/geekbrains/lesson4/Controller.java
+++ src/main/java/geekbrains/lesson4/Controller.java
@@ -1,4 +1,19 @@
 package geekbrains.lesson4;
 
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+    public void sendAuthMsg() {
+
+    }
+
+    public void sendMsg() {
+
+    }
+
+    public void clientsListClicked(MouseEvent mouseEvent) {
+
+    }
+
 }
