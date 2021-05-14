bb8c7685a0bf9e4d5d85bcb0ed95774befe947a2
Pavel.Gonyaev <P.Gonyaev@exite.ru>
2021-05-14 16:16:43 +0300

    Изменение

--- src/main/java/geekbrains/lesson6/EchoClient.java
+++ src/main/java/geekbrains/lesson6/EchoClient.java
@@ -1,5 +1,7 @@
 package geekbrains.lesson6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -7,32 +9,50 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class EchoClient {
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
 
     public static void main(String[] args) {
-        try (Socket socket = new Socket(EchoConstants.SERVER_ADDR, EchoConstants.SERVER_PORT);
-             Scanner scanner = new Scanner(socket.getInputStream());
-             PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);) {
-            System.out.println("Server connected");
-            KeyListener keyListener = new KeyListener(printWriter);
-            keyListener.setDaemon(true);
-            keyListener.start();
-            while (true) {
-                String str = "";
-                try {
-                    str = scanner.nextLine();
-                } catch (NoSuchElementException e) {
-                    break;
-                }
-                ;
-                if (str.equals("/end")) {
-                    System.out.println("Server disconnect");
-                    break;
-                }
-                System.out.println("Server: " + str);
-            }
+        new EchoClient();
+    }
+
+    public EchoClient() {
+        try {
+            openConnection();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public void openConnection() throws IOException {
+        socket = new Socket(EchoConstants.SERVER_ADDR, EchoConstants.SERVER_PORT);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        PrintWriter printWriter = new PrintWriter(out, true);
+
+        KeyListener keyListener = new KeyListener(printWriter);
+        keyListener.setDaemon(true);
+        keyListener.start();
+
+        sendMessage();
+    }
+
+    public void sendMessage() {
+        Scanner scanner = new Scanner(in);
+        while (true) {
+            String str = "";
+            try {
+                str = scanner.nextLine();
+            } catch (NoSuchElementException e) {
+                break;
+            }
+            ;
+            if (str.equals("/end")) {
+                System.out.println("Server disconnect");
+                break;
+            }
+            System.out.println("Server: " + str);
+        }
+    }
 }
--- src/main/java/geekbrains/lesson6/EchoConstants.java
+++ src/main/java/geekbrains/lesson6/EchoConstants.java
@@ -3,4 +3,5 @@ package geekbrains.lesson6;
 public class EchoConstants {
     public static final String SERVER_ADDR = "localhost";
     public static final int SERVER_PORT = 8189;
+    public static final String STOP_WORD = "/end";
 }
