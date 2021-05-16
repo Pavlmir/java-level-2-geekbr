6f41a24f2166905d99793b7279754ac18d12c0b8
pavlmir <mirprost@gmail.com>
2021-05-16 15:31:04 +0300

    Урок 7. Написание сетевого чата. Часть I

---
+++ src/main/java/geekbrains/lesson7/ChatConstants.java
@@ -0,0 +1,14 @@
+package geekbrains.lesson7;
+
+public class ChatConstants {
+
+    public static final String HOST = "localhost";
+
+    public static final int PORT = 8190;
+
+    public static final String AUTH_COMMAND = "/auth";
+
+    public static final String AUTH_OK = "/authok";
+
+    public static final String STOP_WORD = "/end";
+}
---
+++ src/main/java/geekbrains/lesson7/ServerApp.java
@@ -0,0 +1,10 @@
+package geekbrains.lesson7;
+
+/**
+ * Класс для запуска приложения
+ */
+public class ServerApp {
+    public static void main(String[] args) {
+        new MyServer();
+    }
+}
